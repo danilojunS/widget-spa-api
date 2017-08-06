07659f14a3faaf7cf73fa6dcc434eecd1396d45c
Danilo Jun <danilojunS@users.noreply.github.com>
2017-08-06 03:47:53 -0300

    added widget business entity

---
+++ business/entities/widget/widget.go
@@ -0,0 +1,31 @@
+package widget
+
+import "errors"
+
+// Widget business entity
+type Widget struct {
+	ID        int
+	Name      string
+	Color     string
+	Price     string
+	Inventory int
+	Melts     bool
+}
+
+// Validate a widget
+func (w Widget) Validate() error {
+	if w.ID == 0 {
+		return errors.New("id is a required field and must not be 0")
+	}
+	if w.Name == "" {
+		return errors.New("name is a required field and must not be empty string")
+	}
+	if w.Color == "" {
+		return errors.New("color is a required field and must not be empty string")
+	}
+	if w.Price == "" {
+		return errors.New("price is a required field and must not be empty string")
+	}
+
+	return nil
+}
