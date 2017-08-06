175a9817d04039c720e9ee456f8309a3ee608cc2
Danilo Jun <danilojunS@users.noreply.github.com>
2017-08-06 18:22:02 -0300

    added Clear method to user repository mock

--- business/repositories/user/user_repository_test.go
+++ business/repositories/user/user_repository_test.go
@@ -1,8 +1,9 @@
-package user_test
+package userRepo_test
 
 import (
 	user "github.com/danilojunS/widgets-spa-api/business/entities/user"
 	userRepo "github.com/danilojunS/widgets-spa-api/business/repositories/user"
+	utils "github.com/danilojunS/widgets-spa-api/utils"
 	"testing"
 )
 
@@ -18,12 +19,12 @@ func TestUserCreate(t *testing.T) {
 	u := user.User{Name: userFixtureName, Gravatar: userFixtureGravatar}
 	var err error
 	createdUserID, err = userRepo.Create(u)
-	checkError(err)
+	utils.CheckError(err)
 }
 
 func TestUserRead(t *testing.T) {
 	users, err := userRepo.Read(createdUserID)
-	checkError(err)
+	utils.CheckError(err)
 
 	if len(users) != 1 {
 		t.Error("Should find one created user")
@@ -41,7 +42,7 @@ func TestUserUpdate(t *testing.T) {
 	const newGravatar string = "https://d.js/awesome-gravatar"
 	createdUser.Gravatar = newGravatar
 	updatedUserID, err := userRepo.Update(createdUser)
-	checkError(err)
+	utils.CheckError(err)
 
 	if updatedUserID != createdUser.ID {
 		t.Error("Should update correct user")
@@ -49,7 +50,7 @@ func TestUserUpdate(t *testing.T) {
 	}
 
 	users, err := userRepo.Read(createdUser.ID)
-	checkError(err)
+	utils.CheckError(err)
 
 	if len(users) != 1 {
 		t.Error("Should find one updated user")
@@ -65,7 +66,7 @@ func TestUserUpdate(t *testing.T) {
 
 func TestDeleteUser(t *testing.T) {
 	deletedUserID, err := userRepo.Delete(updatedUser)
-	checkError(err)
+	utils.CheckError(err)
 
 	if deletedUserID != updatedUser.ID {
 		t.Error("Should delete correct user")
@@ -73,15 +74,37 @@ func TestDeleteUser(t *testing.T) {
 	}
 
 	users, err := userRepo.Read(deletedUserID)
-	checkError(err)
+	utils.CheckError(err)
 
 	if len(users) > 0 {
 		t.Error("Should not find deleted user")
 	}
 }
 
-func checkError(err error) {
-	if err != nil {
-		panic(err)
+func TestClearUsers(t *testing.T) {
+	users := []user.User{
+		{Name: "user1", Gravatar: "gravatar1"},
+		{Name: "user2", Gravatar: "gravatar2"},
+		{Name: "user3", Gravatar: "gravatar3"},
 	}
+
+	for _, user := range users {
+		_, err := userRepo.Create(user)
+		utils.CheckError(err)
+	}
+
+	err := userRepo.Clear()
+	utils.CheckError(err)
+
+	usersFromRepo, err := userRepo.Read(0)
+	utils.CheckError(err)
+
+	if len(usersFromRepo) > 0 {
+		t.Error("Should clear all users in repository")
+	}
+}
+
+func TestTeardown(t *testing.T) {
+	err := userRepo.Clear()
+	utils.CheckError(err)
 }
