ca6985b69e80b9c48d31065880294cad6f907c54
Danilo Jun <danilojunS@users.noreply.github.com>
2017-08-07 23:54:38 -0300

    added error utils for handlers

---
+++ infra/web/handlers/errors_utils.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func buildErrorResolver(message string, status int) func(w http.ResponseWriter, customMessage string) {
+	return func(w http.ResponseWriter, customMessage string) {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(status)
+
+		if customMessage != "" {
+			message = customMessage
+		}
+
+		_, err := w.Write([]byte(message))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+// InternalError handles unexpected server errors
+var InternalError = buildErrorResolver("Internal server error", http.StatusInternalServerError)
+
+// ValidationError handles parameters validation erros
+var ValidationError = buildErrorResolver("Params validation error", http.StatusBadRequest)
+
+// NotFoundError handles not found errors
+var NotFoundError = buildErrorResolver("Not found", http.StatusNotFound)
