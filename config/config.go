7af747418ffc76eae65f532aa97867d7e1281703
Danilo Jun <danilojunS@users.noreply.github.com>
2017-08-09 03:15:42 -0300

    added default values for config

--- config/config.go
+++ config/config.go
@@ -11,12 +11,12 @@ import (
 // Config struct
 type Config struct {
 	Port        int    `env:"PORT" envDefault:"4000"`
-	Auth        bool   `env:"AUTH"`
-	TokenSecret string `env:"TOKEN_SECRET"`
-	DB          string `env:"DB"`
+	Auth        bool   `env:"AUTH" envDefault:"true"`
+	TokenSecret string `env:"TOKEN_SECRET" envDefault:"super secret"`
+	DB          string `env:"DB" envDefault:"postgres"`
 	DBUser      string `env:"DB_USER"`
 	DBPass      string `env:"DB_PASS"`
-	DBMock      bool   `env:"DB_MOCK"`
+	DBMock      bool   `env:"DB_MOCK" envDefault:"true"`
 }
 
 // Init environment variables
